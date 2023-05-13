53aa73efa7c9f198d310a50eabef23ad3e132d63
shubham.k@techouts.com <shubham.k@techouts.com>
2023-05-13 23:30:00 +0530

    Wrapperclass

---
+++ com.practisejava/src/com/practisejavaprograms/Wraperclass.java
@@ -0,0 +1,16 @@
+package com.practisejavaprograms;
+
+public class Wrapperclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		// Integer num1=new Integer(9); boxing creating object for Integer class
+		Integer num1=9; // auto-boxing 
+		System.out.println("Valuue by Auto-boxing ="+ num1);
+		
+        Integer num2=num1.intValue(); //un-boxing
+        System.out.println("Valuue by Un-boxing= "+num2);
+	} 
+
+}
